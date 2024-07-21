9b9c0032a1971bdc0c37e09fa1ba2545b12b6689
Varun Dixit <dixitvarun0208@gmail.com>
2024-07-21 12:39:31 +0530

    Your commit message here

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      "lh3.googleusercontent.com",
+      "cdn.pixabay.com",
+      "yt3.ggpht.com",
+      "p16-amd-va.tiktokcdn.com",
+      "image.shutterstock.com",
+      "yt3.googleusercontent.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ utils/constants.tsx
@@ -0,0 +1,46 @@
+import { BsCode } from "react-icons/bs";
+import { GiCakeSlice, GiGalaxy, GiCampCookingPot } from "react-icons/gi";
+import { FaPaw, FaMedal, FaGamepad } from "react-icons/fa";
+import { AiOutlineFileExcel } from "react-icons/ai";
+import { CgCardSpades } from "react-icons/cg";
+import { FaPaperPlane } from "react-icons/fa";
+import { BiBasketball } from "react-icons/bi";
+
+export const topics = [
+  {
+    name: "development",
+    icon: <BsCode />,
+  },
+  {
+    name: "excel",
+    icon: <AiOutlineFileExcel />,
+  },
+  {
+    name: "gaming",
+    icon: <FaGamepad />,
+  },
+  {
+    name: "cooking",
+    icon: <GiCampCookingPot />,
+  },
+  {
+    name: "dance",
+    icon: <GiGalaxy />,
+  },
+  {
+    name: "origami",
+    icon: <FaPaperPlane />,
+  },
+  {
+    name: "cards",
+    icon: <CgCardSpades />,
+  },
+  {
+    name: "basketball",
+    icon: <BiBasketball />,
+  },
+];
+
+export const footerList1 = [];
+export const footerList2 = [];
+export const footerList3 = [];
